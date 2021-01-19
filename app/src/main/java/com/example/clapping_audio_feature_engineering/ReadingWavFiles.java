82942f08194aa61592d34f464cc6b22d03875a54
Junnie Lee <heunghari@naver.com>
2021-01-19 16:40:20 +0900

    uploaded the audio files

--- app/src/main/java/com/example/clapping_audio_feature_engineering/ReadingWavFiles.java
+++ app/src/main/java/com/example/clapping_audio_feature_engineering/ReadingWavFiles.java
@@ -6,4 +6,30 @@ package com.example.clapping_audio_feature_engineering;
 
 public class ReadingWavFiles {
 
+    private static final double MAX_16_BIT = Short.MAX_VALUE; // 32,767
+
+    // 고민거리
+    // ** constructor 에서 모든 데이터들에 대해 아래 과정이 다 되도록 만들까?!
+
+
+    // (1) convert wav file-> byte array
+    private static void read_wav() {
+
+    }
+
+
+    // (2) chunk them in 50ms size window
+
+
+    // (3) convert byte array -> to double array
+    private static double[] read(byte[]file) {
+        byte[] data = new byte[file.length];
+        int N = data.length;
+        double[] d = new double[N/2];
+        for (int i = 0; i < N/2; i++) {
+            d[i] = ((short) (((data[2*i+1] & 0xFF) << 8) + (data[2*i] & 0xFF))) / ((double) MAX_16_BIT);
+        }
+        return d;
+    }
+
 }
