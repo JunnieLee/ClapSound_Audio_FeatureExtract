933fc3c7fccee31e78f24f9173b6bfa8b0d2105a
Junnie Lee <heunghari@naver.com>
2021-01-21 17:39:31 +0900

    changed into byte->short->double due to the 16-bit audio file format / chunked in windows

--- app/src/main/java/com/example/clapping_audio_feature_engineering/ReadingWavFiles.java
+++ app/src/main/java/com/example/clapping_audio_feature_engineering/ReadingWavFiles.java
@@ -18,11 +18,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 
 public class ReadingWavFiles {
 
     private static final double MAX_16_BIT = Short.MAX_VALUE; // 32,767
+    private static final int SAMPLE_RATE = 48000;
+    private static final int ELEMENT_NUM_PER_WINDOW = 2400;
+    // 모두 4초 미만의 오디오 데이터이므로, 한 오디오 파일 당 max window num은 (48000*4) / 2400 = 80
+    private static final int MAX_WINDOW_NUM_PER_FILE = 80;
+
+    // 고민: 오디오파일 1~10까지 하나의 array 에 담을 것인가 (fancier code) 아니면 10개의 variable 을 따로 만들것인가(좀 노가다스럽지만 복잡도 감소)
+
+
 
     // ** 고민거리 : constructor 에서 모든 데이터들에 대해 아래 과정이 다 되도록 만들까?!
 
@@ -49,40 +59,49 @@ public class ReadingWavFiles {
         return byteData;
     }
 
+    // 16bit PCM audio file이기 때문에 byte array -> short array -> double array 로 바꿔줘야함
 
-    // (2) convert byte array -> to double array
-    private static double[] read(byte[]file) {
-        byte[] data = new byte[file.length];
-        int N = data.length;
-        double[] d = new double[N/2];
-        for (int i = 0; i < N/2; i++) {
-            d[i] = ((short) (((data[2*i+1] & 0xFF) << 8) + (data[2*i] & 0xFF))) / ((double) MAX_16_BIT);
+    // (2) convert byte array -> to short array
+    private static short[] Byte2Short(byte[] bytes) {
+        short[] out = new short[bytes.length / 2]; // will drop last byte if odd number
+        ByteBuffer bb = ByteBuffer.wrap(bytes);
+        for (int i = 0; i < out.length; i++) {
+            out[i] = bb.getShort();
         }
-        return d;
+        return out;
     }
 
-    // (3) chunk the extracted array in 50ms size window
-    // --> (sample rate * 0.05) 개씩 element 를 뽑아내서 나누면 됨!
+    // (3) convert short array -> to double array
+    private static double[] Short2Double(short[] arr) {
+        double[] out = new double[arr.length]; // will drop last byte if odd number
+        for (int i = 0; i < out.length; i++) {
+            out[i] = (double) arr[i];
+        }
+        return out;
+    }
 
+
+    // (4) chunk the extracted array in 50ms size window
+    // --> (sample rate * 0.05) = ELEMENT_NUM_PER_WINDOW 개씩 element 를 뽑아내서 나누면 됨!
+    // 따라서 output 은 array of double arrays 형태가 됨.
+    // --> 각 audio file 의 길이에 따라 해당 array의 길이도 달라질 수 있음. (element 크기는 ELEMENT_NUM_PER_WINDOW로 동일)
+
+    private static double[][] ChunkByWindows(double[] arr){
+        double[][] result = new double[MAX_WINDOW_NUM_PER_FILE][ELEMENT_NUM_PER_WINDOW];
+        int loop_cnt = arr.length/ ELEMENT_NUM_PER_WINDOW; // loop를 몇번돌지 (chunk 몇개를 만들지) 결정
+        int cursor = 0;
+        for (int i=0; i<loop_cnt; i++) {
+            // (ex) 4개씩 나누고 싶으면 a[0]~a[3], a[4]~a[7]
+            result[i] = Arrays.copyOfRange(arr, cursor, cursor + ELEMENT_NUM_PER_WINDOW -1 );
+            cursor++;
+        }
+        return result;
+    }
 
 
 
     // HELPER FUNCTIONS // --------------------------------------------------------------------
 
-    // get sample rate for calculating window size for 50ms chunk
-    private int getSampleRate(String path) throws IOException {
-        MediaExtractor mex = new MediaExtractor();
-        try {
-            mex.setDataSource(path);// the addresss location of the sound on sdcard.
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        MediaFormat mf = mex.getTrackFormat(0);
-        // int bitRate = mf.getInteger(MediaFormat.KEY_BIT_RATE);
-        int sampleRate = mf.getInteger(MediaFormat.KEY_SAMPLE_RATE);
-        return sampleRate;
-    }
 
 
 }
